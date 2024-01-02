a71b6790546d296629aab3ca48cdd24de98d3668
nov03 <ishii@serverworks.co.jp>
2024-01-02 04:06:02 +0000

    asdfasdfasdfasdfasdfsadfasdf

--- test/pytest_sample_test.py
+++ test/pytest_sample_test.py
@@ -12,13 +12,12 @@ from pytest_sample import lambda_handler
 def test_lambda_handler_true():
     event = {'flg1': True, 'flg2': False}
     result = lambda_handler(event, None)
-    assert result['statusCode'] == 200
-    assert result['body'] == 'Hello from Lambda!'
-    # statement_coverageの挙動を検証するための追加のアサーションをここに記述
+    assert result['statusCode'] == 200, "Status code should be 200"
+    assert result['body'] == 'Hello from Lambda!', "Response body should be 'Hello from Lambda!'"
 
-# def test_lambda_handler_false():
-#     event = {'flg1': False, 'flg2': False}
-#     result = lambda_handler(event, None)
-#     assert result['statusCode'] == 200
-#     assert result['body'] == 'Hello from Lambda!'
-#     # statement_coverageの挙動を検証するための追加のアサーションをここに記述
+def test_lambda_handler_false():
+    event = {'flg1': False, 'flg2': False}
+    result = lambda_handler(event, None)
+    assert result['statusCode'] == 200, "Status code should be 200"
+    assert result['body'] == 'Hello from Lambda!', "Response body should be 'Hello from Lambda!'"
+
